962b19cf484dd5fe3b8de1197bd3149b202718b2
tomasarede <tomas.arede@gmail.com>
2025-12-08 23:11:42 +0000

    capital to lower

--- project/schema.sql
+++ project/schema.sql
@@ -1,14 +1,11 @@
-
-
-CREATE TABLE Location 
-(
-long NUMERIC(9,6),
-lat NUMERIC(8,6),
-name VARCHAR(255) NOT NULL,
-PRIMARY KEY(long, lat)
+CREATE TABLE location (
+    long NUMERIC(9,6),
+    lat NUMERIC(8,6),
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(long, lat)
 );
 
-CREATE TABLE Country(
+CREATE TABLE country (
 	iso_code VARCHAR(5),
 	name VARCHAR(60) NOT NULL,
 	flag VARCHAR(2083) NOT NULL,
@@ -17,231 +14,190 @@ CREATE TABLE Country(
 	PRIMARY KEY(iso_code)
 );
 
-CREATE TABLE Class(
+CREATE TABLE class (
 	name VARCHAR(80),
 	max_length NUMERIC(6,2) NOT NULL,
 	PRIMARY KEY(name),
     UNIQUE(name, max_length) -- primary key makes this combination unique, but its written explicitly for the dbms to accept it as a foreign key
 );
 
-CREATE TABLE Boat -- TYPE!
-(
-cni VARCHAR(25), --
-picture_path VARCHAR(2083) NOT NULL,
-length NUMERIC(6,2) NOT NULL,
-name VARCHAR(255) NOT NULL,
-class_name VARCHAR(80),
-registered_iso VARCHAR(5),
-class_max_length NUMERIC(6,2),
-year INTEGER NOT NULL,
-PRIMARY KEY(cni),
-FOREIGN KEY (class_name, class_max_length) REFERENCES Class(name,max_length),
-FOREIGN KEY (registered_iso) REFERENCES Country(iso_code),
-CHECK (length <= class_max_length) --IC15
+CREATE TABLE boat (
+    cni VARCHAR(25),
+    picture_path VARCHAR(2083) NOT NULL,
+    length NUMERIC(6,2) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    class_name VARCHAR(80),
+    registered_iso VARCHAR(5),
+    class_max_length NUMERIC(6,2),
+    year INTEGER NOT NULL,
+    PRIMARY KEY(cni),
+    FOREIGN KEY (class_name, class_max_length) REFERENCES class(name,max_length),
+    FOREIGN KEY (registered_iso) REFERENCES country(iso_code),
+    CHECK (length <= class_max_length) --IC15
 );
 
-CREATE TABLE Sailor -- IC1
-(
-sid SERIAL,
-first_name VARCHAR(50) NOT NULL,
-surname VARCHAR(80) NOT NULL,
-email VARCHAR(254) NOT NULL,
-PRIMARY KEY(sid),
-UNIQUE(email) --IC1
--- Every sailor must be either Junior or Senior
--- (CAN NOT BE BOTH)
+CREATE TABLE sailor (
+    sid SERIAL,
+    first_name VARCHAR(50) NOT NULL,
+    surname VARCHAR(80) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    PRIMARY KEY(sid),
+    UNIQUE(email) --IC1
+    -- Every sailor must be either Junior or Senior
+    -- (CAN NOT BE BOTH)
 );
 
-CREATE TABLE Junior_Sailor
-(
-sid INTEGER,
-PRIMARY KEY(sid),
-FOREIGN KEY(sid) REFERENCES Sailor(sid)
+CREATE TABLE junior_sailor (
+    sid INTEGER,
+    PRIMARY KEY(sid),
+    FOREIGN KEY(sid) REFERENCES sailor(sid)
 );
 
-CREATE TABLE Senior_Sailor
-(
-sid INTEGER,
-PRIMARY KEY(sid),
-FOREIGN KEY(sid) REFERENCES Sailor(sid)
+CREATE TABLE senior_sailor (
+    sid INTEGER,
+    PRIMARY KEY(sid),
+    FOREIGN KEY(sid) REFERENCES sailor(sid)
 );
 
-CREATE TABLE Certification 
-(
-sid INTEGER,
-issue_date DATE,
-expiry_date DATE NOT NULL,
-PRIMARY KEY(sid, issue_date),
-FOREIGN KEY(sid) REFERENCES Sailor(sid),
-CHECK (issue_date < expiry_date) -- IC17
--- IC9:
--- The sailor that is skipper for a trip must
--- hold a certification for the class of the boat
--- used on the reservation which the trip is associated to.
+CREATE TABLE certification (
+    sid INTEGER,
+    issue_date DATE,
+    expiry_date DATE NOT NULL,
+    PRIMARY KEY(sid, issue_date),
+    FOREIGN KEY(sid) REFERENCES sailor(sid),
+    CHECK (issue_date < expiry_date) -- IC17
+    -- IC9:
+    -- The sailor that is skipper for a trip must
+    -- hold a certification for the class of the boat
+    -- used on the reservation which the trip is associated to.
 );
 
-CREATE TABLE Reservation 
-(
-cni VARCHAR(25),
-start_date DATE,
-end_date DATE NOT NULL,
-PRIMARY KEY(cni, start_date),
-UNIQUE(cni, start_date, end_date), -- primary key makes this combination unique, but its written explicitly for the dbms to accept it as a foreign key
-FOREIGN KEY(cni) REFERENCES Boat(cni),
-CHECK(start_date <= end_date) --IC11
-
---- IC12:
---- Different reservations that use the same boat can
---- not have overlapping intervals of start_date and end_date.
---- (CAN NOT DO IT WITHOUT TRIGGERS - comparing different rows)
-
---- IC16:
---- The reservation start_date year must be bigger or equal than
--- the year the boat used was registered in a country.
-
+CREATE TABLE reservation (
+    cni VARCHAR(25),
+    start_date DATE,
+    end_date DATE NOT NULL,
+    PRIMARY KEY(cni, start_date),
+    UNIQUE(cni, start_date, end_date), -- primary key makes this combination unique, but its written explicitly for the dbms to accept it as a foreign key
+    FOREIGN KEY(cni) REFERENCES boat(cni),
+    CHECK(start_date <= end_date) --IC11
+    --- IC12:
+    --- Different reservations that use the same boat can
+    --- not have overlapping intervals of start_date and end_date.
+    --- (CAN NOT DO IT WITHOUT TRIGGERS - comparing different rows)
 );
 
-
-CREATE TABLE authorized_for 
-(
-cni VARCHAR(25),
-start_date DATE,
-sid INTEGER,
-PRIMARY KEY(cni, start_date, sid),
-FOREIGN KEY(sid) REFERENCES Sailor(sid),
-FOREIGN KEY(cni, start_date) REFERENCES Reservation(cni, start_date)
--- Every reservation must have at least one sailor
+CREATE TABLE authorized_for (
+    cni VARCHAR(25),
+    start_date DATE,
+    sid INTEGER,
+    PRIMARY KEY(cni, start_date, sid),
+    FOREIGN KEY(sid) REFERENCES sailor(sid),
+    FOREIGN KEY(cni, start_date) REFERENCES reservation(cni, start_date)
+    -- Every reservation must have at least one sailor
 );
 
-CREATE TABLE responsible_for
-(
-cni VARCHAR(25),
-start_date DATE,
-responsible_sid INTEGER,
-PRIMARY KEY(cni, start_date, responsible_sid),
-FOREIGN KEY (cni, start_date, responsible_sid)
-    REFERENCES authorized_for(cni, start_date, sid),  -- IC4 - enforces the person is authorized for that reservation
-FOREIGN KEY (cni, start_date) REFERENCES Reservation(cni, start_date),
-FOREIGN KEY (responsible_sid) REFERENCES Senior_Sailor(sid)
--- Every reservation must have at least one responsible senior sailor
+CREATE TABLE responsible_for (
+    cni VARCHAR(25),
+    start_date DATE,
+    responsible_sid INTEGER,
+    PRIMARY KEY(cni, start_date, responsible_sid),
+    FOREIGN KEY (cni, start_date, responsible_sid)
+        REFERENCES authorized_for(cni, start_date, sid),  -- IC4 - enforces the person is authorized for that reservation
+    FOREIGN KEY (cni, start_date) REFERENCES reservation(cni, start_date),
+    FOREIGN KEY (responsible_sid) REFERENCES senior_Sailor(sid)
+    -- Every reservation must have at least one responsible senior sailor
 );
 
-CREATE TABLE Trip 
-(
-cni VARCHAR(25),
-start_date DATE,
-end_date DATE,
-take_off_date DATE,
-arrival_date DATE NOT NULL,
-ins_ref INTEGER NOT NULL,
-is_skipper_for_sid INTEGER,
-is_skipper_for_start_date DATE,
-is_skipper_for_cni VARCHAR(25),
-PRIMARY KEY(cni, start_date, take_off_date),
-FOREIGN KEY (cni, start_date, end_date) REFERENCES Reservation(cni,start_date,end_date),
-FOREIGN KEY (is_skipper_for_sid, is_skipper_for_start_date, is_skipper_for_cni)
-    REFERENCES authorized_for(sid, start_date, cni),
-CHECK(cni = is_skipper_for_cni), --IC5
-CHECK(start_date = is_skipper_for_start_date), --IC5
-CHECK(take_off_date BETWEEN start_date AND end_date), -- IC6
-CHECK(arrival_date BETWEEN start_date AND end_date), --IC6
-CHECK(take_off_date <= arrival_date) -- IC13
+CREATE TABLE trip (
+    cni VARCHAR(25),
+    start_date DATE,
+    end_date DATE,
+    take_off_date DATE,
+    arrival_date DATE NOT NULL,
+    ins_ref INTEGER NOT NULL,
+    is_skipper_for_sid INTEGER,
+    is_skipper_for_start_date DATE,
+    is_skipper_for_cni VARCHAR(25),
+    PRIMARY KEY(cni, start_date, take_off_date),
+    FOREIGN KEY (cni, start_date, end_date) REFERENCES reservation(cni,start_date,end_date),
+    FOREIGN KEY (is_skipper_for_sid, is_skipper_for_start_date, is_skipper_for_cni)
+        REFERENCES authorized_for(sid, start_date, cni),
+    CHECK(cni = is_skipper_for_cni), --IC5
+    CHECK(start_date = is_skipper_for_start_date), --IC5
+    CHECK(take_off_date BETWEEN start_date AND end_date), -- IC6
+    CHECK(arrival_date BETWEEN start_date AND end_date), --IC6
+    CHECK(take_off_date <= arrival_date) -- IC13
 
--- IC9:
---- A sailor who is the skipper for a trip must hold,
--- for every jurisdiction recorded on that trip, a certification that both
--- (a) enables that jurisdiction and (b) enables a class with max_length
--- greater or equal than the length of the boat used on the reservation
--- to which the trip is associated.
+    -- IC9:
+    --- A sailor who is the skipper for a trip must hold,
+    -- for every jurisdiction recorded on that trip, a certification that both
+    -- (a) enables that jurisdiction and (b) enables a class with max_length
+    -- greater or equal than the length of the boat used on the reservation
+    -- to which the trip is associated.
 
 
---- IC10:
---- The sailor that is skipper for a trip must hold a certification whose
--- valid period (issue_date to expiry_date) fully covers that trip's duration
--- (take_off_date to arrival_date).
+    --- IC10:
+    --- The sailor that is skipper for a trip must hold a certification whose
+    -- valid period (issue_date to expiry_date) fully covers that trip's duration
+    -- (take_off_date to arrival_date).
 
---- IC14:
---- Different trips associated to the same reservation must not have overlapping
---- take_off_date - arrival_date and start_date - end_date intervals.
---- (NEED TO COMPARE ROWS. NEED TRIGGERS TO DO THAT!)
+    --- IC14:
+    --- Different trips associated to the same reservation must not have overlapping
+    --- take_off_date - arrival_date and start_date - end_date intervals.
+    --- (NEED TO COMPARE ROWS. NEED TRIGGERS TO DO THAT!)
 );
 
+CREATE TABLE starts_ends (
+    cni VARCHAR(25),
+    start_date DATE,
+    take_off_date DATE,
+    start_long NUMERIC(9,6),
+    end_long NUMERIC(9,6),
+    start_lat NUMERIC(8,6),
+    end_lat NUMERIC(8,6),
+    PRIMARY KEY (cni, start_date, take_off_date, start_long, start_lat, end_long, end_lat),
+    FOREIGN KEY (cni,start_date,take_off_date) REFERENCES trip(cni,start_date,take_off_date),
+    FOREIGN KEY (start_long,start_lat) REFERENCES location(long,lat),
+    FOREIGN KEY (end_long, end_lat) REFERENCES location(long,lat)
+    -- Every trip must have at least one start and end location.
 
--- WOULD WORK AND WOULD BE MUCH CLEANERRRRR
---CREATE TABLE Trip
---(
---  cni                 VARCHAR(25),
---  start_date          DATE       ,
---  take_off_date       DATE ,
--- arrival_date        DATE  NOT NULL       ,
---  ins_ref             INTEGER     NOT NULL,
---  skipper_sid         INTEGER    ,
---  PRIMARY KEY (cni, start_date, take_off_date),
---  FOREIGN KEY (cni, start_date) REFERENCES Reservation(cni, start_date),
---  FOREIGN KEY (cni, start_date, skipper_sid)
---    REFERENCES authorized_for(cni, start_date, sid),
---);
---
-
-
-CREATE TABLE starts_ends -- MANDATORY!
-(
-cni VARCHAR(25),
-start_date DATE,
-take_off_date DATE,
-start_long NUMERIC(9,6),
-end_long NUMERIC(9,6),
-start_lat NUMERIC(8,6),
-end_lat NUMERIC(8,6),
-PRIMARY KEY (cni, start_date, take_off_date, start_long, start_lat, end_long, end_lat),
-FOREIGN KEY (cni,start_date,take_off_date) REFERENCES Trip(cni,start_date,take_off_date),
-FOREIGN KEY (start_long,start_lat) REFERENCES Location(long,lat),
-FOREIGN KEY (end_long, end_lat) REFERENCES Location(long,lat)
--- Every trip must have at least one start and end location.
-
--- IC7:
--- Every pair of rows must be at least 1 nautical mile apart
--- (We can not model this with a single simple CHECK because
--- it needs to compare a row to other rows -> NEED TRIGGERS)
+    -- IC7:
+    -- Every pair of rows must be at least 1 nautical mile apart
+    -- (We can not model this with a single simple CHECK because
+    -- it needs to compare a row to other rows -> NEED TRIGGERS)
 );
 
-
-
-
-
-
-CREATE TABLE Jurisdiction(
+CREATE TABLE jurisdiction (
 	name VARCHAR(80),
 	PRIMARY KEY(name)
     -- Every jurisdiction is either International or National
     -- (CAN NOT BE BOTH)
 );
 
-CREATE TABLE enables(
+CREATE TABLE enables (
     sid INTEGER,
     issue_date DATE,
     jurisdiction_name VARCHAR(80),
     class_name VARCHAR(80),
     PRIMARY KEY (sid, issue_date, jurisdiction_name, class_name),
-    FOREIGN KEY (sid, issue_date) REFERENCES Certification(sid, issue_date),
-    FOREIGN KEY (jurisdiction_name) REFERENCES Jurisdiction(name),
-    FOREIGN KEY (class_name) REFERENCES Class(name)
+    FOREIGN KEY (sid, issue_date) REFERENCES certification(sid, issue_date),
+    FOREIGN KEY (jurisdiction_name) REFERENCES jurisdiction(name),
+    FOREIGN KEY (class_name) REFERENCES class(name)
     -- Every certification must have at least one Jurisdiction defined
 );
 
-CREATE TABLE International_Jurisdiction(
+CREATE TABLE international_jurisdiction(
 	name VARCHAR(80),
 	PRIMARY KEY(name),
-	FOREIGN KEY(name) REFERENCES Jurisdiction(name)
+	FOREIGN KEY(name) REFERENCES jurisdiction(name)
 );
 
-CREATE TABLE National_Jurisdiction(
+CREATE TABLE national_jurisdiction(
 	name VARCHAR(80),
 	administrated_by_iso VARCHAR(5),
 	PRIMARY KEY(name),
-	FOREIGN KEY(name) REFERENCES Jurisdiction(name),
-	FOREIGN KEY(administrated_by_iso) REFERENCES Country(iso_code)
-
+	FOREIGN KEY(name) REFERENCES jurisdiction(name),
+	FOREIGN KEY(administrated_by_iso) REFERENCES country(iso_code)
 );
 
 CREATE TABLE records(
@@ -251,19 +207,18 @@ CREATE TABLE records(
     sequence INTEGER,
     jurisdiction_name VARCHAR(80),
     PRIMARY KEY(cni, start_date, take_off_date, jurisdiction_name),
-    FOREIGN KEY(cni, start_date, take_off_date) REFERENCES Trip(cni, start_date, take_off_date),
-    FOREIGN KEY(jurisdiction_name) REFERENCES Jurisdiction(name)
+    FOREIGN KEY(cni, start_date, take_off_date) REFERENCES trip(cni, start_date, take_off_date),
+    FOREIGN KEY(jurisdiction_name) REFERENCES jurisdiction(name)
     -- Every trip must have at least one jurisdiction recorded
 );
-
 
 CREATE TABLE define(
     long NUMERIC(9,6),
     lat NUMERIC(8,6),
     country_name VARCHAR(60),
     PRIMARY KEY (long,lat),
-    FOREIGN KEY (long,lat) REFERENCES Location(long,lat),
-    FOREIGN KEY (country_name) REFERENCES Country(name)
+    FOREIGN KEY (long,lat) REFERENCES location(long,lat),
+    FOREIGN KEY (country_name) REFERENCES country(name)
     -- IC8:
     -- Every country that registers a boat must have
     -- at least one location defined.
